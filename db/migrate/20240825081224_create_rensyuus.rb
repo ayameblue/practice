134cd7a8fb2eae7f7037ecefb7471201c3cb34db
ayameblue <0af72-3.325635p@ezweb.ne.jp>
2024-08-25 17:14:46 +0900

    rensyuuテーブル作成

--- db/migrate/20240825081224_create_rensyuus.rb
+++ db/migrate/20240825081224_create_rensyuus.rb
@@ -1,6 +1,9 @@
 class CreateRensyuus < ActiveRecord::Migration[7.0]
   def change
     create_table :rensyuus do |t|
+      t.string :name
+      t.string :text
+      t.text :image
 
       t.timestamps
     end
